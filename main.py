e66875ab405b766be47a2fab2b7c3304e3eccda7
Llczff <44291288+Llczff@users.noreply.github.com>
2023-01-04 06:38:01 +0700

    edit-main

--- main.py
+++ main.py
@@ -42,11 +42,6 @@ class User(BaseModel):
     lastname: str
 
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
 class TokenData(BaseModel):
     email: Union[str, None] = None
 
